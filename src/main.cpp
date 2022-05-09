cde174582e5ef85d67efa47b3fff243cb412191a
arthurtakase <arthur.decaen@epitech.eu>
2022-05-09 12:42:10 +0200

    FEAT: modification fichiers clang

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
-#include "raylib.h"
 #include <iostream>
 
-int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+#include "raylib.h"
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-    EndDrawing();
-  }
-  CloseWindow(); // Close window and OpenGL context
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-  return 0;
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow(); // Close window and OpenGL context
+
+    return 0;
 }

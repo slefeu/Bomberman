fe205f23b39575c74bd25324ebd69de1977a1c2c
arthurtakase <arthur.decaen@epitech.eu>
2022-05-09 12:51:35 +0200

    FEAT: draft for camera

---
+++ include/Engine/Tools/Camera.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-400-BDX-4-1-indiestudio-arthur.decaen
+** File description:
+** Camera
+*/
+
+#ifndef CAMERA_HPP_
+#define CAMERA_HPP_
+
+class Camera
+{
+  public:
+    Camera();
+    ~Camera();
+
+  protected:
+  private:
+};
+
+#endif /* !CAMERA_HPP_ */

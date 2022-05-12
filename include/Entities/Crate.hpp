93233636bf4e80283c475ceadbab19aaad9e4b2b
arthurtakase <arthur.decaen@epitech.eu>
2022-05-12 16:27:00 +0200

    STYLE: better crate

--- include/Entities/Crate.hpp
+++ include/Entities/Crate.hpp
@@ -8,25 +8,14 @@
 #ifndef CRATE_HPP_
 #define CRATE_HPP_
 
-#include "Entities.hpp"
+#include "Box.hpp"
 
-class Crate : public Entities
+class Crate : public Box
 {
   public:
     Crate(Vector3 pos) noexcept;
     ~Crate() noexcept;
-    void    display() noexcept;
-    void    action(std::vector<std::unique_ptr<Entities>>& others) noexcept;
-    Vector3 getPosition() noexcept;
-    Vector3 getSize() noexcept;
-    bool    isColliding(std::vector<std::unique_ptr<Entities>>& others) noexcept;
-    bool    isCollidingNextTurn(std::vector<std::unique_ptr<Entities>>& others, int xdir, int zdir) noexcept;
-    bool    update() noexcept;
-
-  private:
-    void moveX(float x) noexcept;
-    void moveY(float y) noexcept;
-    void moveZ(float z) noexcept;
+    void display() noexcept;
 };
 
 #endif /* !CRATE_HPP_ */
--- src/Entities/Crate.cpp
+++ src/Entities/Crate.cpp
@@ -8,16 +8,18 @@
 #include "Crate.hpp"
 
 Crate::Crate(Vector3 pos) noexcept
+    : Box(pos, { 1.0f, 1.0f, 1.0f })
 {
-    position   = pos;
-    type       = EntityType::CRATE;
-    color      = ORANGE;
-    hitbox     = std::make_unique<HitBox>((Vector3){ position.x, 0.35f, position.z }, (Vector3){ 0.7f, 0.7f, 0.7f }, true);
-    scale      = 0.012f;
-    model      = LoadModel("Assets/Models/box.obj");
-    texture    = LoadTexture("Assets/Textures/box.png");
-    position.y = 0 - scale;
-    isEnable   = true;
+    type             = EntityType::CRATE;
+    hitbox->position = { position.x, 0.35f, position.z };
+    hitbox->size     = { 0.7f, 0.7f, 0.7f };
+    scale            = 0.012f;
+    model            = LoadModel("Assets/Models/box.obj");
+    texture          = LoadTexture("Assets/Textures/box.png");
+    position.y       = 0 - scale;
+    isEnable         = true;
+
+    hitbox->update(hitbox->position);
 
     model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
 }
@@ -33,54 +35,4 @@ void Crate::display() noexcept
     if (!isEnable) return;
     DrawModel(model, position, scale, WHITE);
     hitbox->display();
-}
-
-void Crate::moveX(float x) noexcept
-{
-    position.x += x * GetFrameTime();
-}
-
-void Crate::moveY(float y) noexcept
-{
-    position.y += y * GetFrameTime();
-}
-
-void Crate::moveZ(float z) noexcept
-{
-    position.z += z * GetFrameTime();
-}
-
-void Crate::action(std::vector<std::unique_ptr<Entities>>& others) noexcept
-{
-    (void)others;
-    return;
-}
-
-Vector3 Crate::getPosition() noexcept
-{
-    return position;
-}
-
-Vector3 Crate::getSize() noexcept
-{
-    return size;
-}
-
-bool Crate::isColliding(std::vector<std::unique_ptr<Entities>>& others) noexcept
-{
-    (void)others;
-    return false;
-}
-
-bool Crate::isCollidingNextTurn(std::vector<std::unique_ptr<Entities>>& others, int xdir, int zdir) noexcept
-{
-    (void)others;
-    (void)xdir;
-    (void)zdir;
-    return false;
-}
-
-bool Crate::update(void) noexcept
-{
-    return false;
 }

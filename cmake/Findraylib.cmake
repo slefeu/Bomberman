ee5cb5fc61a7463a5e9c1dd3e3523917ed9a9f87
Solène Lefeu <solene.lefeu@epitech.eu>
2022-06-10 11:37:55 +0200

    FIX: organisation of files and folders

--- cmake/Findraylib.cmake
+++ cmake/Findraylib.cmake
@@ -1,16 +1,15 @@
 message(STATUS "Find Raylib...")
-# find_package(raylib 4.0.0 QUIET)
-if (NOT raylib_FOUND) # If there's none, fetch and build raylib
+if (NOT raylib_FOUND)
   include(FetchContent)
   FetchContent_Declare(
     raylib
     URL https://github.com/raysan5/raylib/archive/refs/tags/4.0.0.tar.gz
   )
   FetchContent_GetProperties(raylib)
-  if (NOT raylib_POPULATED) # Have we downloaded raylib yet?
+  if (NOT raylib_POPULATED)
     set(FETCHCONTENT_QUIET NO)
     FetchContent_Populate(raylib)
-    set(BUILD_EXAMPLES OFF CACHE BOOL "" FORCE) # don't build the supplied examples
+    set(BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
     add_subdirectory(${raylib_SOURCE_DIR} ${raylib_BINARY_DIR})
   endif()
 endif()

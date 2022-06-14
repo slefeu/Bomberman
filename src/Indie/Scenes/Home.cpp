89a9858bf503e5142d54bc9a22574728c094ca1d
Solène Lefeu <solene.lefeu@epitech.eu>
2022-06-14 16:38:54 +0200

    ADD: Handling of all buttons in menu with controller

--- src/Indie/Scenes/Home.cpp
+++ src/Indie/Scenes/Home.cpp
@@ -159,13 +159,17 @@ void Home::createButtons() noexcept
 void Home::action() noexcept
 {
     if (controller.isGamepadConnected(0)) {
-        if (controller.isGamepadButtonPressed(0, G_Button::G_DPAD_UP))
-            button_index_ = (button_index_ - 1) % buttons_.size();
-        if (controller.isGamepadButtonPressed(0, G_Button::G_DPAD_DOWN))
-            button_index_ = (button_index_ + 1) % buttons_.size();
+        if (controller.isGamepadButtonPressed(0, G_Button::G_DPAD_UP) button_index > 0)
+            button_index_ = button_index_ - 1;
+        if (controller.isGamepadButtonPressed(0, G_Button::G_DPAD_DOWN) && button_index < buttons_.size() + settings_.size())
+            button_index_ = button_index_ + 1;
         if (controller.isGamepadButtonPressed(0, G_Button::G_B)) buttons_[button_index_].action();
         for (auto& it : buttons_) it.setState(0);
-        buttons_[button_index_].setState(1);
+        for (auto& it : settings_) it.setState(0);
+        if (button_index_ > buttons_.size()) {
+            settings_[button_index_ - buttons_.size()].setState(1);
+        } else
+            buttons_[button_index_].setState(1);
     } else {
         for (auto& it : buttons_)
             if (it.checkCollision(core_entry_.getData().getMouseHandler())) { it.action(); }
